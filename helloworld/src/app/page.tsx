5ab634ded5916ea248ae1402380cc315e4e42d42
muhammadabrar02 <abrarsolangi2002@gmail.com>
2024-10-31 23:00:36 +0500

    Hello world

---
+++ helloworld/src/app/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex justify-center mt-[15%] bg-white p-5 drop-shadow(0 1px 2px rgb(0 0 0 / 0.1)) outline shadow-[5px_5px_15px_rgba(85,84,84,0.3)]
+">
+      <div className="font-bold text-[rgb(94,94,95)] text-[56px] p-5 text-center">
+        Hello World
+      </div>
+
+    </div>
+
+
+  );
+}
