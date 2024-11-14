4a68898a38b1259cd6dafe168d58cea826907432
muhammadabrar02 <abrarsolangi2002@gmail.com>
2024-11-14 23:24:27 +0500

    updated

--- saas-landing-web/src/app/page.tsx
+++ saas-landing-web/src/app/page.tsx
@@ -72,7 +72,10 @@ export default function Home() {
       {/* WORK-MANAGEMENT */}
       <div className="w-full h-[1588px] top-[921px] left-[1px] p-[140px_220px_140px_220px] gap-[100px] opacity-0">
 
-       
+        <div className="w-full max-w-[1480px] min-h-[547px] gap-[60px] opacity-0">
+          
+        </div>
+
 
 
       </div>
