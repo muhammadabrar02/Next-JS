4cf079b1cbc3cc7dc40581936b8f9d5463a238f9
muhammadabrar02 <abrarsolangi2002@gmail.com>
2024-11-18 00:26:29 +0500

    updated

--- e-commerce-website-webpage/src/app/page.tsx
+++ e-commerce-website-webpage/src/app/page.tsx
@@ -3,11 +3,19 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      {/* NAVBAR */}
+      <div className="sm:w-full h-[34px] p-2 bg-black flex items-center justify-center overflow-x-hidden">
+  <p className="w-[301px] h-[16px] gap-0 text-white justify-center flex font-satoshi text-[12px] font-normal leading-[16.2px] text-left md:">
+    Sign up and get 20% off to your first order.
+    <span className="font-satoshi text-[12px] font-medium leading-[16.2px] text-left underline decoration-solid custom-span text-white">
+      Sign up now
+    </span>
+  </p>
+</div>
 
-      {/* second section  */}
 
-      {/* third section  */}
+
+
+
     </div>
 
   );
