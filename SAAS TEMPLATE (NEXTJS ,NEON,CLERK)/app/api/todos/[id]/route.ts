3980b82dbdad393debe0f1546e05cd914095ba07
muhammadabrar02 <abrarsolangi2002@gmail.com>
2026-01-10 02:05:42 +0500

    updated

---
+++ SAAS TEMPLATE (NEXTJS ,NEON,CLERK)/app/api/todos/[id]/route.ts
@@ -0,0 +1,46 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { auth } from '@clerk/nextjs/server';
+import prisma from '@/lib/prisma';
+import { todo } from 'node:test';
+
+export async function DELETE(req: NextRequest,
+    { params }: { params: { id: string } }) {
+
+    const { userId } = auth()
+
+    if (!userId) {
+        return NextResponse.json({ message: 'Unauthorized' }, { status: 401 })
+    }
+
+    try {
+        const todoId = params.id
+        await prisma.todo.findUnique({
+            where: { id: todoId }
+        })
+
+        if (!todo) {
+            return NextResponse.json({ error: "Todo not found" }, { status: 401 });
+
+        }
+
+        if (todo.userId !== userId) {
+            return NextResponse.json({ error: "Forbidden" }, { status: 403 })
+        }
+
+        await prisma.todo.delete({
+            where: { id: todoId }
+        })
+
+        return NextResponse.json({ message: "Todo deleted Successfully" }, { status: 200 })
+
+
+    } catch (err) {
+        console.error('Error updating subscription', err)
+        return NextResponse.json(
+            { error: "Internal Server Error" },
+            { status: 500 }
+        )
+
+    }
+
+}
---
+++ SAAS TEMPLATE (NEXTJS ,NEON,CLERK)/app/api/todos/route.ts
@@ -0,0 +1,104 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { auth } from '@clerk/nextjs/server';
+import prisma from '@/lib/prisma';
+import { title } from 'process';
+import { NestedMiddlewareError } from 'next/dist/build/utils';
+
+const ITEMS_PER_PAGE = 10;
+export async function GET(req: NextRequest) {
+
+    const { userId } = auth()
+
+    if (!userId) {
+        return NextResponse.json({ message: 'Unauthorized' }, { status: 401 })
+
+    }
+
+    const { searchParams } = new URL(req.url)
+    const page = parseInt(searchParams.get("page") || "1")
+    const search = searchParams.get("search") || ""
+
+    try {
+        const todos = await prisma.todo.findMany({
+            where: {
+                userId,
+                title: {
+                    contains: search,
+                    mode: "insensitive"
+                }
+            },
+            orderBy: { createdAt: "desc" },
+            take: ITEMS_PER_PAGE,
+            skip: (page - 1) * ITEMS_PER_PAGE
+        })
+
+        const totalTodos = await prisma.todo.count({
+            where: {
+                userId,
+                title: {
+                    contains: "search",
+                    mode: "insensitive"
+                }
+            }
+        })
+
+        const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE)
+
+        return NextResponse.json({
+            todos,
+            currentPage: page,
+            totalPages
+        })
+
+    } catch (err) {
+        console.error('Error fetching todos', err)
+        return NextResponse.json(
+            { error: "Internal Server Error" },
+            { status: 500 }
+        )
+
+    }
+
+
+}
+export async function POST(req: NextRequest) {
+
+    const { userId } = auth()
+
+    if (!userId) {
+        return NextResponse.json({ message: 'Unauthorized' }, { status: 401 })
+
+    }
+
+    const user = await prisma.user.findUnique({
+        where: { id: userId },
+        include: { todos: true }
+    })
+    console.log(user);
+
+    if (!user) {
+        return NextResponse.json(
+            { message: 'User not found' },
+            { status: 404 }
+        )
+    }
+
+    if(!user.isSubscribed && user.todos.length >= 3){ 
+        return NextResponse.json({
+            error: "Free users  can only create up to 3 todos. Please subscribe to our paid plans to write more awesome todos."
+        },{ status: 403 })
+    }
+
+    const {title} = await req.json( ) 
+    const todo =await prisma.todo.create({
+        data: {
+            title,
+            userId
+        }
+    })
+
+    return NextResponse.json(todo, { status: 201 } )
+
+
+
+}
